1b62de527f39e66d5656080e2122892c825095d9
alreadynyeong <kjw87130@naver.com>
2023-05-15 00:11:49 +0900

    [docs] title 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,14 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Nav from '@/components/layout/Nav'
 import Bottom from '@/components/layout/Bottom'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>GDSC-SSWU-4th</title>
+      </Head>
       <Nav/>
       <Component {...pageProps}/>
       <Bottom/>
