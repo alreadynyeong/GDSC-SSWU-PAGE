49c3cab467bd909c5995041b235acfdb7196e722
alreadynyeong <kjw87130@naver.com>
2023-06-02 20:51:35 +0900

    [style] 스타일 변경

--- components/layout/Nav.tsx
+++ components/layout/Nav.tsx
@@ -1,95 +1,103 @@
-import React, { useState } from "react"
-import { NavBarMenus } from "@/constants/Route"
-import styled from "styled-components"
-import SSWU from '@/public/Logo/SSWU.svg'
-import Link from "next/link"
-import { Mobile, PC } from "@/hook/useMideaQuery"
+import React, { useState } from "react";
+import { NavBarMenus } from "@/constants/Route";
+import styled from "styled-components";
+import SSWU from "@/public/Logo/SSWU.svg";
+import Link from "next/link";
+import { Mobile, PC } from "@/hook/useMideaQuery";
 
 const Container = styled.div`
-    padding: 50px;
-    background-color: white;
-    width: 100%;
-    height: 130px;
-    display: flex;
-	justify-content: space-between;
-    position: fixed;
-    box-shadow: 5px 0.2px 5px 1px lightgray;
-    overflow: scroll;
-`
+  padding: 50px;
+  background-color: white;
+  width: 100%;
+  height: 130px;
+  display: flex;
+  justify-content: space-between;
+  position: fixed;
+  box-shadow: 5px 0.2px 5px 1px lightgray;
+  overflow: scroll;
+`;
+
 const MenuContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-item: center;
-    gap: 50px;
-    padding-left: 50px;
-`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 50px;
+  padding-left: 50px;
+`;
+
 const Menu = styled.div`
-    width: fit-content;
-    height: fit-content;
-    display: flex;
-    align-items: center;
-    border-radius: 51px;
-    border: 1px solid;
-    padding: 10px;
-`
+  width: fit-content;
+  height: fit-content;
+  display: flex;
+  align-items: center;
+  border-radius: 51px;
+  border: 1px solid;
+  padding: 10px;
+`;
+
 const MobileMenuDrop = styled.div`
-    // border: solid 1px black;
-    text-align: right;
-    height: fit-content;
-    padding: 5px;
-    position: absolute;
-    top: 7px;
-    right: 40px;
-    margin-right: -40px;
-`
+  text-align: right;
+  height: fit-content;
+  padding: 5px;
+  position: absolute;
+  top: 7px;
+  right: 40px;
+  margin-right: -40px;
+  z-index: 10;
+  background: white;
+`;
+
 const Nav = () => {
-    const [menu, setMenu] = useState<boolean>(false);
-    return (
-        <>
-            <Container>
+  const [menu, setMenu] = useState<boolean>(false);
+
+  const toggleMenu = () => {
+    setMenu(!menu);
+  };
+
+  return (
+    <>
+      <Container>
         <div>
-            <SSWU width={199} height={'100%'}/>
+          <SSWU width={199} height={"100%"} />
         </div>
         <Mobile>
-            {menu ? null : 
-            <div onClick={()=>setMenu(true)}>
-                MENU
-            </div>}
-            {menu ? 
+          {menu ? null : (
+            <div onClick={toggleMenu}>MENU</div>
+          )}
+          {menu ? (
             <MobileMenuDrop>
-                <div onClick={()=>setMenu(false)}>
-                    X
+              <div onClick={toggleMenu}>X</div>
+              {NavBarMenus.map((menu) => (
+                <div
+                  style={{ borderColor: menu.color }}
+                  key={menu.route}
+                >
+                  <Link href={menu.route} passHref>
+                    <div>{menu.title}</div>
+                    <hr></hr>
+                  </Link>
                 </div>
-            {NavBarMenus.map((menu)=>(
-                    <div
-                    style={{borderColor: menu.color}}  
-                    key={menu.route}>
-                        <Link href={menu.route} passHref>
-                            <div>{menu.title}</div>
-                            <hr></hr>
-                        </Link>
-                    </div>
-                ))}
+              ))}
             </MobileMenuDrop>
-            : null}
-
+          ) : null}
         </Mobile>
         <PC>
-            <MenuContainer>
-                {NavBarMenus.map((menu)=>(
-                    <Menu
-                    style={{borderColor: menu.color}}  
-                    key={menu.route}>
-                        <Link href={menu.route} passHref>
-                            <div>{menu.title}</div>
-                        </Link>
-                    </Menu>
-                ))}
-            </MenuContainer>
+          <MenuContainer>
+            {NavBarMenus.map((menu) => (
+              <Menu
+                style={{ borderColor: menu.color }}
+                key={menu.route}
+              >
+                <Link href={menu.route} passHref>
+                  <div>{menu.title}</div>
+                </Link>
+              </Menu>
+            ))}
+          </MenuContainer>
         </PC>
-    </Container>
-        </>
-    )
-}
+      </Container>
+    </>
+  );
+};
 
 export default Nav;
