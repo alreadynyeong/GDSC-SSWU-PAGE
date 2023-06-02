fd82457f74e7075d8cd3a94c8f499ca36be345da
alreadynyeong <kjw87130@naver.com>
2023-06-02 21:25:36 +0900

    [feat] home bottom 반응형 추가

--- components/home/HomeMiddle.tsx
+++ components/home/HomeMiddle.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import HomeImage from "@/public/Home/HomeImage.svg"
 import HomeImage2 from "@/public/Home/HomeImage2.svg"
 import HomeImage3 from "@/public/Home/HomeImage3.svg"
 import { HomeParts } from "@/constants/Part";
+import { Mobile, PC } from "@/hook/useMideaQuery";
 
 const Container = styled.div`
     height: 680px;
@@ -36,20 +37,68 @@ const PartTitle = styled.div`
     font-weight: semi-bold;
     margin: 30px;
 `
+const MobileTitle = styled.div`
+    font-size: 38px;
+    font-weight: bold;
+    padding: 90px 20px 20px 20px;
+    color: #0F9D58;
+`
+const PickDiv = styled.div`
+    display: flex;
+    gap: 7px;
+    margin: 0 auto;
+    width: fit-content;
+`
+const PickCircle = styled.div`
+    border: 1px solid #0F9D58;
+    width: 10px;
+    height: 10px;
+    border-radius: 100px;
+    background-color: ${({ isActive }) => (isActive ? "#0F9D58" : "transparent")};
+`
+const MobilePartBox = styled.div`
+    align-item: center;
+    width: fit-content;
+    margin: 0 auto;
+    margin-top: 40px;
+
+`
 
 const HomeMiddle = () => {
+    const [pick, setPick] = useState<number>(0);
     return (
         <Container>
-            <Title>WE STUDY ABOUT..</Title>
-            <PartBox>
-                {HomeParts.map((p)=>(
-                    <Part key={p.title}>
-                        {p.svg==1?(<HomeImage/>):p.svg==2?(<HomeImage2/>):(<HomeImage3/>)}
-                        <PartTitle>{p.title}</PartTitle>
-                        <p>{p.content}</p>
+            <PC>
+                <Title>WE STUDY ABOUT..</Title>
+                <PartBox>
+                    {HomeParts.map((p, index) => (
+                        <Part key={p.title}>
+                            {p.svg==1?(<HomeImage/>):p.svg==2?(<HomeImage2/>):(<HomeImage3/>)}
+                            <PartTitle>{p.title}</PartTitle>
+                            <p>{p.content}</p>
+                        </Part>
+                    ))}
+                </PartBox>
+            </PC>
+            <Mobile>
+                <MobileTitle>WE STUDY ABOUT..</MobileTitle>
+                <PickDiv>
+                    {HomeParts.map((p, index) => (
+                        <PickCircle
+                            key={index}
+                            onClick={() => setPick(index)}
+                            isActive={pick === index}
+                        />
+                    ))}
+                </PickDiv>
+                <MobilePartBox>
+                    <Part key={HomeParts[pick].title}>
+                        {HomeParts[pick].svg==1?(<HomeImage/>):HomeParts[pick].svg==2?(<HomeImage2/>):(<HomeImage3/>)}
+                        <PartTitle>{HomeParts[pick].title}</PartTitle>
+                        <p>{HomeParts[pick].content}</p>
                     </Part>
-                ))}
-            </PartBox>
+                </MobilePartBox>
+            </Mobile>
         </Container>
     )
 }
