90f654d3e73d255103c50ed6d56bc74904b18ee9
alreadynyeong <kjw87130@naver.com>
2023-06-03 16:38:11 +0900

    [feat]home bottom 반응형 적용

--- components/home/HomeBottom.tsx
+++ components/home/HomeBottom.tsx
@@ -1,3 +1,4 @@
+import { Mobile, PC } from "@/hook/useMideaQuery";
 import React from "react";
 import styled from "styled-components";
 
@@ -43,23 +44,69 @@ const Title = styled.p`
     font-weight: bold;
 
 `
+const MobileContentBox = styled.div`
+    width: fit-contents;
+    margin: 0 auto;
+    align-item: center;
+`
+const MobileImageContainer = styled.div`
+    width: 328px;
+    height: 335px;
+    border-radius: 100px 0px 0px 0px;
+    overflow: hidden;
+`
+const MobileTextBox = styled.div`
+    width: 300px;
+    margin-bottom: 50px;
+    >p{
+        padding-top: 20px;
+        padding-left:10px;
+    }
+`
+const MobileSquareImage = styled.div`
+    width: 280px;
+    height: 77px;
+    margin: 0 auto;
+    overflow: hidden;
+    margin-top: -70px;
+`
 
 const HomeBottom = () => {
     return (
-        <Container>
-            <ContentBox>
-                <ImageContainer>
-                    <img src={`https://alreadynyeong.github.io/GDSC-SSWU-PAGE/Photo/Highlight2.jpeg`} alt={""} width={328} height={335}/>
-                </ImageContainer>
-                <TextBox>
-                    <Title>HIGHLIGHT</Title>
-                    <p>GDSC 성신여대에서는 GDSC SSWU뿐만 아니라 GDG, GDSC 주최의 다양한 행사에 참여합니다.</p>
-                </TextBox>
-            </ContentBox>
-            <SquareImage>
-                <img src={`https://alreadynyeong.github.io/GDSC-SSWU-PAGE/Photo/Highlight1.jpeg`} alt={""} width={515} height={117}/>
-            </SquareImage>
-        </Container>
+        <>
+        <PC>
+            <Container>
+                <ContentBox>
+                    <ImageContainer>
+                        <img src={`https://alreadynyeong.github.io/GDSC-SSWU-PAGE/Photo/Highlight2.jpeg`} alt={""} width={328} height={335}/>
+                    </ImageContainer>
+                    <TextBox>
+                        <Title>HIGHLIGHT</Title>
+                        <p>GDSC 성신여대에서는 GDSC SSWU뿐만 아니라 GDG, GDSC 주최의 다양한 행사에 참여합니다.</p>
+                    </TextBox>
+                </ContentBox>
+                <SquareImage>
+                    <img src={`https://alreadynyeong.github.io/GDSC-SSWU-PAGE/Photo/Highlight1.jpeg`} alt={""} width={515} height={117}/>
+                </SquareImage>
+            </Container>
+        </PC>
+        <Mobile>
+            <Container>
+                <MobileContentBox>
+                    <MobileTextBox>
+                        <Title>HIGHLIGHT</Title>
+                        <p>GDSC 성신여대에서는 GDSC SSWU뿐만 아니라 GDG, GDSC 주최의 다양한 행사에 참여합니다.</p>
+                    </MobileTextBox>
+                    <MobileImageContainer>
+                        <img src={`https://alreadynyeong.github.io/GDSC-SSWU-PAGE/Photo/Highlight2.jpeg`} alt={""} width={300} height={315}/>
+                    </MobileImageContainer>
+                    <MobileSquareImage>
+                    <img src={`https://alreadynyeong.github.io/GDSC-SSWU-PAGE/Photo/Highlight1.jpeg`} alt={""} width={300} height={70}/>
+                    </MobileSquareImage>
+                </MobileContentBox>
+            </Container>
+        </Mobile>
+        </>
     )
 }
 export default HomeBottom;
