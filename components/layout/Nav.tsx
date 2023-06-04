976a359a8d5e32011befa33d4378da09877eeb0f
alreadynyeong <kjw87130@naver.com>
2023-06-04 23:29:18 +0900

    [style] Nav 스타일 수정

--- components/layout/Nav.tsx
+++ components/layout/Nav.tsx
@@ -21,7 +21,7 @@ const Container = styled.div`
   box-shadow: 5px 0.2px 5px 1px lightgray;
   background-color: ${(props) => props.theme.backgroundColor};
   margin-top: -10px;
-  margin-left: -9px;
+  margin-left: -8px;
 `;
 const MenuContainer = styled.div`
   display: flex;
